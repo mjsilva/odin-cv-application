e0b5a60137cd4d12ea35c8f3f6e8f16bf131b33a
Manuel Silva <manuel@mjs.me>
2023-01-19 18:28:21 +0700

    Initial commit
    - Cleaned up create-react-app
    - Added some CV fields
    - Setup state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "Manuel Silva",
+    };
+  }
+
+  onChangeFormField(e) {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <div className="logo">CV Application</div>
+        </header>
+        <div className="mainContainer">
+          <section className="editor">
+            <div className="personalDetails">
+              <div className="formElement">
+                <label htmlFor="name">Name</label>
+                <input
+                  onChange={(e) => this.onChangeFormField(e)}
+                  value={this.state.name}
+                  type="text"
+                  id="name"
+                  name="name"
+                />
+              </div>
+              <div className="formElement">
+                <label htmlFor="name">Title</label>
+                <input type="text" id="title" name="title" />
+              </div>
+              <div className="formElement">
+                <label htmlFor="mobile">Mobile</label>
+                <input type="text" id="mobile" name="mobile" />
+              </div>
+              <div className="formElement">
+                <label htmlFor="email">Email</label>
+                <input type="text" id="email" name="email" />
+              </div>
+              <div className="formElement">
+                <label htmlFor="about">About</label>
+                <textarea id="about" name="text" />
+              </div>
+            </div>
+          </section>
+          <section className="layout"></section>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
